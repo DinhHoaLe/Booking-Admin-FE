c744889def58dbffbe3d866f5fccda3237808372
Le Dinh Hoa <dinhhoa.0701@gmail.com>
2025-01-17 16:21:33 +0700

    fix Name Store

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { Provider } from "react-redux"; // Import Provider từ react-redux
-import store from "./redux/store"; // Import store đã tạo
+import { Provider } from "react-redux"; 
+import store from "./Redux/Store.jsx"; 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
